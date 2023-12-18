6d2a2f2520a15aa7843c0f65a2e92f9d557d9a6f
Brusmel Gil <Brusmel@outlook.es>
2023-12-18 14:58:58 +0100

    finished exercise

--- src/main/java/dev/brus/imc/App.java
+++ src/main/java/dev/brus/imc/App.java
@@ -1,5 +1,10 @@
 package dev.brus.imc;
 
+import java.util.Scanner;
+
+import dev.brus.imc.model.Calcular;
+import dev.brus.imc.model.Person;
+
 /**
  * Hello world!
  */
@@ -12,6 +17,33 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner dataEntry = new Scanner(System.in);
+
+
+        System.out.println("Introduce tu altura en metros: ");
+        double height1 = dataEntry.nextDouble();
+
+        System.out.println("Introduce tu peso en kilogramos: ");
+        double weight1 = dataEntry.nextDouble();
+
+
+        Person person = new Person(weight1, height1);
+
+        Calcular imcCaculator = new Calcular();
+
+        String result = imcCaculator.calculateIMC(person);
+
+        double calculated = person.getWeight() / Math.pow(person.getHeight(), 2);
+        
+        System.out.println ("Tu IMC es: " + calculated  + result);
+
+
+
+
+        dataEntry.close();
     }
+
+    
+
 }
